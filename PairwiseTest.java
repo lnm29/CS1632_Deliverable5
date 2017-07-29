06e407a3402dfd4cd72dcbc348efbf063a719510
lnm29 <lnm29@pitt.edu>
2017-07-28 20:29:11 -0400

    Update PairwiseTest.java

--- PairwiseTest.java
+++ PairwiseTest.java
@@ -1,58 +1,157 @@
-import java.util.*;
-import org.junit.*;
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+import java.util.*;
+
+/*
+ *  JUnit Tests for Pairwise Class
+ */
+
 public class PairwiseTest {
-	
-	private static final String EOL = System.getProperty("line.separator");
-	
-	//Test that the program exits with 0 parameter
-	@Test
-	public void testNoParameter(){
-		int numArgs = args.length;
-		assertEquals(numArgs, 0);
-	}
-	
-	//Test that the program exits with 1 parameter
-	@Test
-	public void testOneParameter(){
-		int numArgs = args.length;
-		assertEquals(numArgs, 1);
-	}
-	
-	//Test that the program continues with 2 parameters
-	@Test
-	public void testTwoParameter() {
-		int numArgs = args.length;
-		assertEquals(numArgs, 2);
-	}
-	
-	//Test that the program continues with more than 2 parameters
-	@Test
-	public void testMultParameter() {
-		int numArgs = args.length;
-		assertEquals(numArgs, 3);
-	}
-	
-	//Test that the program's name is "Pairwise"
-	@Test
-	public void testProgramName() {
-		
-		assertEquals(Pairwise, new Object());
-	}
-	
-	//Test that the parameter types are boolean
-	@Test
-	public void testParamType() {
-		
-	}
-	
-	//test that the outputs are 1 or 0 for true or false
-	@Test
-	public void testOutput() {
-		
-	}
-	
-	
-	
+    
+    /*
+     *  Helper method to check if two tables are equal
+     */
+    private boolean tableEquals(ArrayList<int[]> table1, ArrayList<int[]> table2) {
+        if (table1.size() != table2.size()) {
+            return false;
+        }
+        for (int i = 0; i < table1.size(); i++) {
+            if (table1.get(i).length != table2.get(i).length) {
+                return false;
+            }
+            for (int j = 0; j < table1.get(i).length; j++) {
+                if (table1.get(i)[j] != table2.get(i)[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    /*
+     *  Test getExhaustiveTable() for n = 1
+     */
+    @Test
+    public void testExhaustiveTableNEquals1() { 
+        int[][] array = {{1},{0}};
+        ArrayList<int[]> expected = new ArrayList<int[]>();
+        for (int[] row: array) {
+            expected.add(row);
+        }
+        assertTrue(tableEquals(Pairwise.getExhaustiveTable(1),expected));
+    }
+    
+    /*
+     *  Test getExhaustiveTable() for n = 2
+     */
+    @Test
+    public void testExhaustiveTableNEquals2() { 
+        int[][] array = {{1,1},{1,0},{0,1},{0,0}};
+        ArrayList<int[]> expected = new ArrayList<int[]>();
+        for (int[] row: array) {
+            expected.add(row);
+        }
+        assertTrue(tableEquals(Pairwise.getExhaustiveTable(2),expected));
+    }
+    
+    /*
+     *  Test getExhaustiveTable() for n = 3
+     */
+    @Test
+    public void testExhaustiveTableNEquals3() { 
+        int[][] array = {{1,1,1},{1,1,0},{1,0,1},{1,0,0},{0,1,1},{0,1,0},{0,0,1},{0,0,0}};
+        ArrayList<int[]> expected = new ArrayList<int[]>();
+        for (int[] row: array) {
+            expected.add(row);
+        }
+        assertTrue(tableEquals(Pairwise.getExhaustiveTable(3),expected));
+    }
+    
+    /*
+     *  Test getExhaustiveTable() for n = 4
+     */
+    @Test
+    public void testExhaustiveTableNEquals4() { 
+        int[][] array = {{1,1,1,1},{1,1,1,0},{1,1,0,1},{1,1,0,0},{1,0,1,1},{1,0,1,0},{1,0,0,1},{1,0,0,0},
+            {0,1,1,1},{0,1,1,0},{0,1,0,1},{0,1,0,0},{0,0,1,1},{0,0,1,0},{0,0,0,1},{0,0,0,0}};
+        ArrayList<int[]> expected = new ArrayList<int[]>();
+        for (int[] row: array) {
+            expected.add(row);
+        }
+        assertTrue(tableEquals(Pairwise.getExhaustiveTable(4),expected));
+    }
+    
+    /*
+     *  Test displayTable() for n = 1
+     */
+    @Test
+    public void testDisplayNEquals1() { 
+        ArrayList<int[]> table = Pairwise.getExhaustiveTable(1);
+        assertTrue(true);
+    }
+    
+    /*
+     *  Test displayTable() for n = 2
+     */
+    @Test
+    public void testDisplayNEquals2() { 
+        ArrayList<int[]> table = Pairwise.getExhaustiveTable(2);
+        assertTrue(true);
+    }
+    
+    /*
+     *  Test displayTable() for n = 3
+     */
+    @Test
+    public void testDisplayNEquals3() { 
+        ArrayList<int[]> table = Pairwise.getExhaustiveTable(3);
+        assertTrue(true);
+    }
+    
+    /*
+     *  Test displayTable() for n = 4
+     */
+    @Test
+    public void testDisplayNEquals4() { 
+        ArrayList<int[]> table = Pairwise.getExhaustiveTable(4);
+        assertTrue(true);
+    }
+    
+    /*
+     *  Test generatePairwiseTable() and checkPairwise() for n = 1
+     */
+    @Test
+    public void testPairwiseNEquals1() { 
+        ArrayList<int[]> table = Pairwise.generatePairwiseTable(new ArrayList<int[]>(),Pairwise.getExhaustiveTable(1));
+        assertTrue(Pairwise.checkPairwise(table));
+    }
+    
+    /*
+     *  Test generatePairwiseTable() and checkPairwise() for n = 2
+     */
+    @Test
+    public void testPairwiseNEquals2() { 
+        ArrayList<int[]> table = Pairwise.generatePairwiseTable(new ArrayList<int[]>(),Pairwise.getExhaustiveTable(2));
+        assertTrue(Pairwise.checkPairwise(table));
+    }
+    
+    /*
+     *  Test generatePairwiseTable() and checkPairwise() for n = 3
+     */
+    @Test
+    public void testPairwiseNEquals3() { 
+        ArrayList<int[]> table = Pairwise.generatePairwiseTable(new ArrayList<int[]>(),Pairwise.getExhaustiveTable(3));
+        assertTrue(Pairwise.checkPairwise(table));
+    }
+    
+    /*
+     *  Test generatePairwiseTable() and checkPairwise() for n = 4
+     */
+    @Test
+    public void testPairwiseNEquals4() { 
+        ArrayList<int[]> table = Pairwise.generatePairwiseTable(new ArrayList<int[]>(),Pairwise.getExhaustiveTable(4));
+        assertTrue(Pairwise.checkPairwise(table));
+    }
 }
